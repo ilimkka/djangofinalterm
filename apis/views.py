24f6550c4ffe5a3651af369e8bb0174e0101dd47
ilimkka <ilimtazhibaev@gmail.com>
2023-12-15 23:16:36 +0600

    final

---
+++ apis/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from rest_framework import generics 
+from blog.models import Post
+from .serializers import PostSerializer
+
+class BlogAPIView(generics.ListAPIView): 
+    queryset = Post.objects.all() 
+    serializer_class = PostSerializer
